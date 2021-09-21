e7dba41264b6ae2d3aa070511b76fd1ef884ed6b
Igor de Freitas de Bairros <igorfbairros@gmail.com>
2021-09-20 22:42:29 -0300

    Add files via upload

---
+++ src/listacircular/src/main/java/one/digitalinovation/Main.java
@@ -0,0 +1,35 @@
+package main.java.one.digitalinovation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("C0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+
+
+        minhaListaCircular.add("C1");
+        System.out.println(minhaListaCircular);
+
+
+        minhaListaCircular.add("C2");
+        minhaListaCircular.add("C3");
+        System.out.println(minhaListaCircular);
+
+
+        /*System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(1));
+        System.out.println(minhaListaCircular.get(2));
+        System.out.println(minhaListaCircular.get(3));*/
+
+        for(int i = 0; i < 20; i++){
+        System.out.println(minhaListaCircular.get(i));
+
+        }
+    }
+}
